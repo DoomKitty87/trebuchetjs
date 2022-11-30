63ecc482d757c256fe80701dd3bb58ec71d01f66
DoomKitty87 <doomkitty887@gmail.com>
2022-11-29 20:43:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var THREE = require('three');
 
 module.exports.addImpulse = function(obj) {
-    if (typeof obj !== 'THREE.Mesh') throw new TypeError('Input must be a threejs mesh.');
-    console.log('Success');
+    //if (typeof obj !== 'THREE.Mesh') throw new TypeError('Input must be a threejs mesh.');
+    console.log(typeof obj);
 }
