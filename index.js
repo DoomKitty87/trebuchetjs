01ef77094fdb2f7c053b6d4b0553f45dc959c4ce
DoomKitty87 <doomkitty887@gmail.com>
2022-11-29 21:20:38 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const THREE = require('three');
 
 module.exports.addImpulse = function(obj, force, direction, world) {
 
-    if (!(obj instanceof THREE.BufferGeometry)) throw new TypeError('Object must be a threejs mesh.');
+    if (!(obj instanceof THREE.Mesh)) throw new TypeError('Object must be a threejs mesh.');
     if (!(typeof force !== 'number')) throw new TypeError('Force must be a number.')
     if (!(direction instanceof THREE.Vector3)) throw new TypeError('Direction must be a (threejs) Vector3.')
     if (!(world instanceof World)) throw new TypeError('World must be a trebuchet.World object.')
