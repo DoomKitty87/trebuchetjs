28f504abc2c92021a13fd1e5cc0502befbdf27dc
DoomKitty87 <doomkitty887@gmail.com>
2022-12-01 17:00:19 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ module.exports.addImpulse = function(obj, force, direction, world) {
     var physicsObj = new PhysicsBody(obj, toApply);
 
     if (world.objects.includes(physicsObj)) {
-        world.objects[world.objects.indexOf(physicsObj)].force += physicsObj.force;
+        world.objects[world.objects.indexOf(physicsObj)].force.add(physicsObj.force);
     }
     else {
         world.objects.push(physicsObj);
@@ -24,9 +24,9 @@ module.exports.simulateWorld = function(world) {
     world.objects.forEach(function(object) {
         object.object.position.add(object.force);
         
-        object.force.x = (Math.max(object.force.x - world.drag), 0) * Math.sign(object.force.x);
-        object.force.y = (Math.max(Math.abs(object.force.y) - world.drag), 0) * Math.sign(object.force.y);
-        object.force.z = (Math.max(Math.abs(object.force.z) - world.drag), 0) * Math.sign(object.force.z);
+        object.force.x = (Math.max(Math.abs(object.force.x) - world.drag, 0)) * Math.sign(object.force.x);
+        object.force.y = (Math.max(Math.abs(object.force.y) - world.drag, 0)) * Math.sign(object.force.y);
+        object.force.z = (Math.max(Math.abs(object.force.z) - world.drag, 0)) * Math.sign(object.force.z);
 
         if (object.force.x == 0 && object.force.y == 0 && object.force.z == 0) {
             //world.objects.splice(world.objects.indexOf(object), 1);
