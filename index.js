77a6dc5aabcdfcc3f3917ac2d6025c017a44509d
DoomKitty87 <doomkitty887@gmail.com>
2022-12-01 22:03:47 -0800

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,24 @@ module.exports.addImpulse = function(obj, force, direction, world) {
 
 module.exports.simulateWorld = function(world) {
     world.objects.forEach(function(object) {
+
         object.object.position.add(object.force);
-        
+
+        //Simulate drag
         object.force.x = (Math.max(Math.abs(object.force.x) - world.drag, 0)) * Math.sign(object.force.x);
         object.force.y = (Math.max(Math.abs(object.force.y) - world.drag, 0)) * Math.sign(object.force.y);
         object.force.z = (Math.max(Math.abs(object.force.z) - world.drag, 0)) * Math.sign(object.force.z);
+
+        //Simulate gravity
+        if (world.up_axis == 'z') {
+            object.force.z -= world.gravity;
+        }
+        else if (world.up_axis == 'y') {
+            object.force.y -= world.gravity;
+        }
+        else if (world.up_axis == 'x') {
+            object.force.x -= world.gravity;
+        }
 
         if (object.force.x == 0 && object.force.y == 0 && object.force.z == 0) {
             world.objects.splice(world.objects.indexOf(object), 1);
@@ -41,8 +54,10 @@ module.exports.readPositions = function(world) {
 }
 
 World = class {
-    constructor(drag) {
+    constructor(drag, gravity = 0, up_axis = 'z') {
         this.drag = drag;
+        this.gravity = gravity;
+        this.up_axis = up_axis;
         this.objects = [];
     }
 }
